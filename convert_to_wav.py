c8a3a8c23cc2907d796ddd9ed685b75199f7403e
Ming Liu <nietism@live.com>
2022-07-04 21:27:03 +0800

    Update convert_to_wav.py

--- convert_to_wav.py
+++ convert_to_wav.py
@@ -3,6 +3,7 @@ import soundfile
 
 
 if __name__ == "__main__":
-    y,sr = librosa.load("./SXY.MP3")
-    soundfile.write("./SXY.wav",y,sr)
+    y, sr = librosa.load("./SXY.MP3", sr=16000)
+    # soundfile.write("./SXY.wav", y, sr)
+    soundfile.write("./SXY.wav", y, 16000)
     print("convert to wav format finished!")
